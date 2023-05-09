1a233e0ba1611324c4fd2d15976669693200c071
Phu <anhyeuemlt001@gmail.com>
2023-05-09 13:35:22 +0700

    first commit

--- util/database.js
+++ util/database.js
@@ -1,1 +1,27 @@
-const mysql = require("mysql2");
+const mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://anhyeuemlt001:hoimanchi@cluster0.uhveq97.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((result) => {
+      console.log("Connected");
+      _db = result.db();
+      callback();
+    })
+    .catch((err) => console.log(err));
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
