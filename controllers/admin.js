9b46f995281498bfbea5f13dbd8ace77b863edf4
Phu <anhyeuemlt001@gmail.com>
2023-05-10 22:30:12 +0700

    use mongodb compass handler data

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,14 @@
 const Product = require("../models/product");
-const getDb = require("../util/database");
-
+const getDb = require("../util/database").getDb;
+const mongodb = require("mongodb");
+const ObjectId = mongodb.ObjectId;
 exports.getAddProduct = (req, res, next) => {
-  res.render("admin/edit-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-    editing: false,
+  Product.fetchAll().then((products) => {
+    res.render("admin/edit-product", {
+      pageTitle: "Add Product",
+      path: "/admin/add-product",
+      editing: false,
+    });
   });
 };
 
@@ -14,6 +17,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+  const product = new Product(title, price, description, imageUrl);
+  product
+    .save()
+    .then((result) => {
+      console.log("Create OK");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
   // req.user
   //   .createProduct({
   //     title: title,
@@ -37,11 +48,8 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-  req.user
-    .getProducts({ where: { id: prodId } })
-    // Product.findById(prodId)
-    .then((products) => {
-      const product = products[0];
+  Product.findById(prodId)
+    .then((product) => {
       if (!product) {
         return res.redirect("/");
       }
@@ -61,14 +69,16 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findById(prodId)
-    .then((product) => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
-      return product.save();
-    })
+  console.log("dasd", req.body);
+  const product = new Product(
+    updatedTitle,
+    updatedPrice,
+    updatedImageUrl,
+    updatedDesc,
+    new ObjectId(prodId)
+  );
+  product
+    .save()
     .then((result) => {
       console.log("UPDATED PRODUCT!");
       res.redirect("/admin/products");
@@ -77,8 +87,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  req.user
-    .getProducts()
+  Product.fetchAll()
     .then((products) => {
       res.render("admin/products", {
         prods: products,
